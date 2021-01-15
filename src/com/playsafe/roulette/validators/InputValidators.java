4c79cdd0e0b917437f811d67bcca3831e686b0ce
chinyanga <chinyangaf@gmail.com>
2021-01-15 08:18:31 +0200

    added names to input file and input validator methods

--- src/com/playsafe/roulette/validators/InputValidators.java
+++ src/com/playsafe/roulette/validators/InputValidators.java
@@ -2,4 +2,24 @@ package com.playsafe.roulette.validators;
 
 public class InputValidators {
 
+	public static boolean isValidNumber(String strNum) {
+		try {
+			int betNumber = Integer.valueOf(strNum);
+			return true;
+		} catch (Exception e) {
+			System.out.println("======Invalid bet Number====");
+			return false;
+		}
+	}
+
+	public static boolean isValidBetCost(String strNum) {
+		try {
+			double betCost = Double.valueOf(strNum);
+			return true;
+		} catch (Exception e) {
+			System.out.println("======Invalid bet Number====");
+			return false;
+		}
+	}
+
 }
