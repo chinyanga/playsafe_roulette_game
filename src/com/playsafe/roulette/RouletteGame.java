ac7f1b7f0af9adac279cf7d5c5dd7f36e9c9e3ca
chinyanga <chinyangaf@gmail.com>
2021-01-15 08:24:46 +0200

    added methods to read names from files ,generate random number

--- src/com/playsafe/roulette/RouletteGame.java
+++ src/com/playsafe/roulette/RouletteGame.java
@@ -1,15 +1,63 @@
 package com.playsafe.roulette;
 
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import com.playsafe.roulette.models.Player;
 
 public class RouletteGame {
 	
+	static int programBetNumber = 0;
+	static List<Player> playerList = new ArrayList<>();
+	static List<Player> currentGamePlayers = new ArrayList<>();
+	static boolean isGameOn = true;
+	static boolean playerExist = false;
+	static int controlCounter = 0;
+	
 	public static void main(String[] args) {
-
-		System.out.println("Program works");
+		
+		readPlayersFromFile();
+		programIntro();
 
 	}
+	
+	public static void programIntro() {
+		System.out.println("=====================================================================");
+		System.out.println("\t\tRoulette Game Started ,the sessions will end in 30 seconds");
+		System.out.println("=====================================================================");
+		System.out.println("==================Valid formaties are============================");
+		System.out.println("\t\t\tJack EVEN 9.0\n \t\t\tJack ODD 9.0\n \t\t\tJack 2 9.0");
+		System.out.println("==================================================================");
+	}
+	
+	public static void readPlayersFromFile() {
+		try {
+			BufferedReader in = new BufferedReader(new FileReader("src/input.txt"));
+			String playerDetail = in.readLine();
+			while (playerDetail != null) {
+				System.out.println(playerDetail);
+				if (playerDetail.split(" ").length == 3) {
+					playerList.add(new Player(playerDetail.split(" ")[0], playerDetail.split(" ")[1],
+							Double.valueOf(playerDetail.split(" ")[2]), "", 0.0));
+				} else
+					playerList.add(new Player(playerDetail, 0, 0.0, "", 0.0));
+				playerDetail = in.readLine();
+			}
+			System.out.println("=============\tThere are:\t" + playerList.size() + "\tplayers\t==========");
+			in.close();
+		} catch (Exception e) {
+			System.out.println(e.toString());
+		}
+	}
+	
+	public static void generateRandomNum() {
+		Random randomBetNum = new Random();
+		int betNum = randomBetNum.nextInt(36);
+		programBetNumber = betNum;
+	}
+
 
 }
